48f783e345e13cb71cf55cafc7239a183bdc9d0d
lucahennet <lucahennet@gmail.com>
2026-04-20 11:44:38 +0200

    Add global missing methods

--- scripts/R/02_simulation.R
+++ scripts/R/02_simulation.R
@@ -17,7 +17,7 @@ simulate_MCAR <- function(mat, prop = 0.1) {
   mat_na <- mat
   n <- nrow(mat)
   num_miss <- round(prop * n)
-  
+
   for (j in seq_len(ncol(mat))) {
     # Ensure we don't try to sample more than available rows
     miss_idx <- sample(n, size = min(num_miss, n))
@@ -104,6 +104,9 @@ simulate_msi_missing <- function(
     remaining <- setdiff(seq_len(n), used)
     
     n_mcar <- total_miss - length(used)
+    # n_mcar <- max(0, total_miss - length(used))
+    # if (n_mcar > 0) idx_mcar <- sample(remaining, min(n_mcar, length(remaining)))
+    # else idx_mcar <- integer(0)
     
     idx_mcar <- sample(remaining, n_mcar)
     
@@ -113,4 +116,31 @@ simulate_msi_missing <- function(
   }
   
   mat_na
+}
+
+# need to clean it
+simulate_MCAR_global <- function(data, prop = 0.1) {
+  all_idx <- which(data != Inf, arr.ind = T)
+  rdm_idx <- sample(1:nrow(all_idx), round(nrow(all_idx)*prop))
+  slc_idx <- all_idx[rdm_idx, ]
+  data_res <- data
+  data_res[slc_idx] <- NA
+  # return(list(data_res = data_res, mis_idx = slc_idx))
+  return(data_res)
+}
+
+simulate_MNAR_global <- function (data_c, mis_var = 0.1, var_prop = seq(.3, .6, .1)) {
+  data_mis <- data_c
+  if (is.numeric(mis_var)) var_mis_list <- sample(1:ncol(data_c), round(ncol(data_c)*mis_var))
+  else if (is.character(mis_var)) var_mis_list <- which(colnames(data_c) %in% mis_var)
+  for (i in 1:length(var_mis_list)) {
+    var_idx <- var_mis_list[i]
+    cur_var <- data_mis[, var_idx]
+    cutoff <- quantile(cur_var, sample(var_prop, 1))
+    cur_var[cur_var < cutoff] <- NA
+    data_mis[, var_idx] <- cur_var
+  }
+  mis_idx_df <- which(is.na(data_mis), arr.ind = T)
+  # return (list(data_mis = data_mis, mis_idx_df = mis_idx_df))
+  return(data_mis)
 }
